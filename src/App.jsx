b82010d90f90075e5fd688f9bb05743cac6987fb
Aniruddha Puranik <91326370+Aniruddha-puranik@users.noreply.github.com>
2023-12-04 23:45:46 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import TicTacToe from "./Components/TicTacToe/TicTacToe"
+
+export default function App(){
+  return (
+    <div>
+      <TicTacToe />
+    </div>
+  )
+}
